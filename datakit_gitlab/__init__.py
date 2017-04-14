804b8b6ae078f763301101b4e71e73eb6d42f7cd
Serdar Tumgoren <zstumgoren@gmail.com>
2017-04-14 11:21:40 -0700

    Silence SSL warnings #2

--- datakit_gitlab/__init__.py
+++ datakit_gitlab/__init__.py
@@ -5,3 +5,7 @@ __email__ = 'stumgoren@ap.org'
 __version__ = '0.1.0'
 
 from .commands.integrate import Integrate
+
+# Squelch the horrifically verbose SSL warnings
+import requests
+requests.packages.urllib3.disable_warnings()
