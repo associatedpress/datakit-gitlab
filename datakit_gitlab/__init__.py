4e7351cc640efbcff256b5039628100a41896077
Larry Fenn <lfenn@ap.org>
2026-07-07 14:22:25 -0400

    Bump version: 0.7.1 → 0.7.2

--- datakit_gitlab/__init__.py
+++ datakit_gitlab/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Serdar Tumgoren"""
 __email__ = 'stumgoren@ap.org'
-__version__ = '0.7.1'
+__version__ = '0.7.2'
 
 from .commands.integrate import Integrate
 from .commands import issues
